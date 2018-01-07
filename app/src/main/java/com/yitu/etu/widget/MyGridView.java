24d432da52be08a9743c83b04a93295f1b4185fb
SayByeBye <1354982095@qq.com>
2018-01-07 23:56:42 +0800

    支付，登陆

---
+++ app/src/main/java/com/yitu/etu/widget/MyGridView.java
@@ -0,0 +1,30 @@
+package com.yitu.etu.widget;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.GridView;
+
+/**
+ * TODO this class desription here
+ * <p>
+ * Created by deng meng on 2017/12/21.
+ */
+public class MyGridView extends GridView{
+    public MyGridView(Context context) {
+        super(context);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public MyGridView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        int expandSpec=MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE>>2,MeasureSpec.AT_MOST);
+        super.onMeasure(widthMeasureSpec, expandSpec);
+    }
+}
