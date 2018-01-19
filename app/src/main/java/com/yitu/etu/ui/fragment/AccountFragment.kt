c7459738c63755d97108ea52d0eb51e4f7ca8319
SayByeBye <1354982095@qq.com>
2018-01-19 20:56:43 +0800

    版本修改

--- app/src/main/java/com/yitu/etu/ui/fragment/AccountFragment.kt
+++ app/src/main/java/com/yitu/etu/ui/fragment/AccountFragment.kt
@@ -20,7 +20,6 @@ import io.rong.imlib.model.CSCustomServiceInfo
 import kotlinx.android.synthetic.main.fragment_account_layout.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
-import java.lang.Exception
 
 
 /**
@@ -161,7 +160,6 @@ class AccountFragment : BaseFragment() {
          * 联系客服
          */
         tv_call_service.setOnClickListener {
-            throw Exception("我崩溃看看")
             /**
              * 启动客户服聊天界面。
              *
